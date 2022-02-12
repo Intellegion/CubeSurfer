f629b11e50ee996a99c4eed95c66ee4225dbf31b
Aditya Srinivas <p.adityasrinivas@gmail.com>
2022-02-13 01:08:42 +0530

    Added level path generation

---
+++ CubeSurfer/Assets/Scripts/Level/Chunk.cs
@@ -0,0 +1,50 @@
+using System;
+using UnityEngine;
+
+public enum ChunkType
+{
+    Basic,
+    Obstacle,
+    Slime
+}
+
+public enum Direction
+{
+    Straight,
+    Left,
+    Right
+}
+
+[Serializable]
+public class Chunk 
+{
+    public ChunkType Type;
+
+    [Range(1, 5)]
+    public int ObstacleCount;
+
+    [Range(0, 3)]
+    public int SlimeCount;
+
+    public bool ShouldTurn;
+
+    public Direction TurnDirection;
+
+    public static Chunk GetBasicChunk(Chunk chunk)
+    {
+        chunk.Type = ChunkType.Basic;
+        return chunk;
+    }
+
+    public static Chunk GetObstacleChunk(Chunk chunk)
+    {
+        chunk.Type = ChunkType.Obstacle;
+        return chunk;
+    }
+
+    public static Chunk GetSlimeChunk(Chunk chunk)
+    {
+        chunk.Type = ChunkType.Obstacle;
+        return chunk;
+    }
+}
---
+++ CubeSurfer/Assets/Scripts/Level/LevelGenerator.cs
@@ -0,0 +1,194 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject pathObject;
+
+    [SerializeField]
+    private GameObject obstacleObject;
+
+    [SerializeField]
+    private GameObject archObject;
+
+    private LevelData levelData;
+
+    private Chunk[] chunks;
+
+    private Direction currentDirection;
+    private float currentSpawnX;
+    private float currentSpawnZ;
+
+    private GameObject obstacle;
+    private GameObject path;
+    private GameObject arch;
+
+    private void Start()
+    {
+        chunks = new Chunk[10];
+        currentDirection = Direction.Straight;
+        currentSpawnX = 0;
+        currentSpawnZ = 0;
+
+        GenerateLevel("Test");
+    }        
+
+    public void GenerateLevel(string levelName)
+    {
+        levelData = ScriptableObject.CreateInstance<LevelData>();
+        chunks[0] = Chunk.GetBasicChunk(new Chunk());
+
+        for (int i = 1; i < 10; i++)
+        {
+            chunks[i] = GenerateChunk(i);
+        }
+
+        foreach(Chunk chunk in chunks)
+        {
+            path = Instantiate(pathObject, transform, false);
+            path.transform.position = new Vector3(currentSpawnX, 0, currentSpawnZ);
+            path.transform.rotation = Quaternion.Euler(0, currentDirection == Direction.Straight ? 0 : 90, 0);
+            switch (chunk.Type)
+            {
+                case ChunkType.Obstacle:
+                    {
+
+
+                        break;
+                    }
+
+                case ChunkType.Slime:
+                    {
+
+                        
+                        break;
+                    }
+                default:
+                    {
+                        continue;
+                    }
+            }
+
+            switch (chunk.TurnDirection)
+            {
+                case Direction.Left:
+                    {
+                        arch = Instantiate(archObject, transform, false);
+                        if (currentDirection == Direction.Straight)
+                        {
+                            arch.transform.position = new Vector3(currentSpawnX - 2.5f, 0, currentSpawnZ + 20);
+                            arch.transform.rotation = Quaternion.Euler(new Vector3(90, 0, 0));
+                            currentSpawnX -= 22.5f;
+                            currentDirection = Direction.Left;
+                        }
+                        else if (currentDirection == Direction.Right)
+                        {
+                            arch.transform.position = new Vector3(currentSpawnX + 20, 0, currentSpawnZ + 2.5f);
+                            arch.transform.rotation = Quaternion.Euler(new Vector3(90, 90, 0));
+                            currentSpawnX += 22.5f;
+                            currentDirection = Direction.Straight;
+                        }
+
+                        currentSpawnZ += 22.5f;
+                        break;
+                    }
+                case Direction.Right:
+                    {
+                        arch = Instantiate(archObject, transform, false);
+                        if (currentDirection == Direction.Straight)
+                        {
+                            arch.transform.position = new Vector3(currentSpawnX + 2.5f, 0, currentSpawnZ + 20);
+                            arch.transform.rotation = Quaternion.Euler(new Vector3(90, -90, 0)); 
+                            currentSpawnX += 22.5f;
+                            currentDirection = Direction.Right;
+                        }
+                        else if (currentDirection == Direction.Left)
+                        {
+                            arch.transform.position = new Vector3(currentSpawnX - 20, 0, currentSpawnZ + 2.5f);
+                            arch.transform.rotation = Quaternion.Euler(new Vector3(90, 180, 0));
+                            currentSpawnX -= 22.5f;
+                            currentDirection = Direction.Straight;
+                        }
+
+                        currentSpawnZ += 22.5f;
+                        break;
+                    }
+                default:
+                    {
+                        if (currentDirection == Direction.Straight)
+                        {
+                            currentSpawnZ += 30;
+                        }
+                        else if (currentDirection == Direction.Left)
+                        {
+                            currentSpawnX -= 30;
+                        }
+                        else
+                        {
+                            currentSpawnX += 30;
+                        }
+
+                        break;
+                    }
+            }
+        }
+
+
+        levelData.Chunks = chunks;
+        EditorUtility.SetDirty(levelData);
+        AssetDatabase.CreateAsset(levelData, "Assets/Levels/" + levelName + ".asset");
+        AssetDatabase.SaveAssets();
+    }
+
+    private Chunk GenerateChunk(int index)
+    {
+        Chunk chunk = new Chunk();
+        int random;
+
+        random = Random.Range(1, 3);
+        chunk.Type = (ChunkType)random;
+
+        switch(chunk.Type)
+        {
+            case ChunkType.Obstacle:
+                {
+                    chunk = Chunk.GetObstacleChunk(chunk);
+                    chunk.ObstacleCount = Random.Range(1, 6);
+                    break;
+                }
+            case ChunkType.Slime:
+                {
+                    chunk = Chunk.GetSlimeChunk(chunk);
+                    chunk.ObstacleCount = Random.Range(1, 6);
+                    chunk.SlimeCount = Random.Range(1, 4);
+                    break;
+                }
+        }
+
+        chunk.ShouldTurn = Random.Range(0, 2) == 0 ? true : false;
+
+        if (chunk.ShouldTurn)
+        {
+            chunk.TurnDirection = Random.Range(0, 2) == 0 ? Direction.Left : Direction.Right;
+
+            if (chunks[index - 1].TurnDirection == Direction.Left)
+            {
+                chunk.TurnDirection = Direction.Right;         
+            }
+
+            else if (chunks[index - 1].TurnDirection == Direction.Right)
+            {
+                chunk.TurnDirection = Direction.Left;
+            }
+        }
+        else
+        {
+            chunk.TurnDirection = Direction.Straight;
+        }
+        return chunk;
+    }
+}
